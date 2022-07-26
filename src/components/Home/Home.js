708534ff043fcfeb45fcf3983340b061216f62a1
bellarude <38563973+bellarude@users.noreply.github.com>
2022-07-26 10:17:50 +0200

    resume new

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -22,7 +22,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> PIPPO </strong>
+                <strong className="main-name"> RUDY RIZZO </strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
