da870a1a066c4a64db52da21b5b7b22525d5234b
bellarude <38563973+bellarude@users.noreply.github.com>
2022-07-29 08:12:10 +0200

    resume + small details

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/new_resume_RR.pdf";
+import pdf from "../../Assets/../Assets/rudy_rizzo_resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/bellarude/Portfolio/master/src/Assets/new_resume_RR.pdf";
+  "https://raw.githubusercontent.com/bellarude/Portfolio/master/src/Assets/rudy_rizzo_resume.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
