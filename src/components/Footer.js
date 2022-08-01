32b843267b1ae42a01734c25a4c4c68d940bfb6a
bellarude <38563973+bellarude@users.noreply.github.com>
2022-08-01 14:57:22 +0200

    modified footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,16 +14,16 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Designed and Developed by Rudy Rizzo</h3>
+          <h3>Designed and Developed by Soumyajit Behera</h3>
         </Col>
         <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} RR</h3>
+          <h3>Copyright © {year} SB</h3>
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
             <li className="social-icons">
               <a
-                href="https://github.com/bellarude"
+                href="https://github.com/soumyajit4419"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
@@ -33,7 +33,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://twitter.com/Rudy__Rizzo"
+                href="https://twitter.com/Soumyajit4419"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
@@ -43,7 +43,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://www.linkedin.com/in/rudy-rizzo-tv/"
+                href="https://www.linkedin.com/in/soumyajit4419/"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
@@ -53,7 +53,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://www.instagram.com/bellarude/"
+                href="https://www.instagram.com/s.o.u.m.y.a_j.i.t/"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
